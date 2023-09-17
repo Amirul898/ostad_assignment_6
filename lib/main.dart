56d09cb2495aefb99caabb867dbc276954083ec2
Amirul898 <amirul898amir@gmail.com>
2023-09-17 20:56:38 +0600

    Final code for Assignment 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PhotoGalleryApp());
+  runApp(const PhotoGalleryApp());
 }
 
 class PhotoGalleryApp extends StatelessWidget {
+  const PhotoGalleryApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: PhotoGalleryScreen(),
     );
   }
 }
 
 class PhotoGalleryScreen extends StatefulWidget {
+  const PhotoGalleryScreen({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _PhotoGalleryScreenState createState() => _PhotoGalleryScreenState();
 }
 
@@ -27,16 +32,15 @@ mysnakbar(msg, context){
 
 class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
   final List<String> imageUrls = [
-    'https://example.com/image1.jpg',
-    'https://example.com/image2.jpg',
-    'https://example.com/image3.jpg',
-    'https://example.com/image4.jpg',
-    'https://example.com/image5.jpg',
-    'https://example.com/image6.jpg',
+    'https://images.unsplash.com/photo-1601034913836-a1f43e143611?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    'https://images.unsplash.com/photo-1601034913836-a1f43e143611?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    'https://images.unsplash.com/photo-1601034913836-a1f43e143611?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
   ];
 
   final TextEditingController textController = TextEditingController();
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
 
 
@@ -45,50 +49,50 @@ class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
     return Scaffold(
 
       appBar: AppBar(
-        title: Text('Photo Gallery'),
+        title: const Text('Photo Gallery'),
       ),
 
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           mysnakbar("Photos Uploaded Successfully!" , context);
         },
-        child: Icon(Icons.cloud_upload),
+        child: const Icon(Icons.cloud_upload),
       ),
 
 
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
 
-            Text(
+            const Text(
               'Welcome to the Photo Gallery!',
               style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
             ),
 
 
-            SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
 
 
             TextField(
               controller: textController,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Enter your message',
                 border: OutlineInputBorder(),
               ),
             ),
 
 
-            SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
             GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
                 crossAxisSpacing: 8.0,
                 mainAxisSpacing: 8.0,
               ),
               shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
+              physics: const NeverScrollableScrollPhysics(),
               itemCount: imageUrls.length,
               itemBuilder: (context, index) {
                 return GestureDetector(
@@ -96,8 +100,8 @@ class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
                   child: Column(
                     children: [
                       Image.network(imageUrls[index]),
-                      SizedBox(height: 4.0),
-                      Text('Image ${index + 1}'),
+                      const SizedBox(height: 4.0),
+                      Text('Photo ${index + 1}'),
                     ],
                   ),
                 );
@@ -105,28 +109,60 @@ class _PhotoGalleryScreenState extends State<PhotoGalleryScreen> {
             ),
 
             
-            SizedBox(height: 16.0),
-            Text(
+            const SizedBox(height: 16.0),
+            const Text(
               'Sample Photos:',
               style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 8.0),
+
+            const SizedBox(height: 8.0),
+
+
             ListTile(
-              leading: Icon(Icons.photo),
-              title: Text('Sample Photo 1'),
-              subtitle: Text('Subtitle 1'),
+              leading: ClipOval(
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+                  width: 40, // Adjust the width to your preference
+                  height: 40, // Adjust the height to your preference
+                  fit: BoxFit.cover, // You can choose the BoxFit that fits your needs
+                ),
+              ),
+              title: const Text('Photo 1'),
+              subtitle: const Text('Description for photo 1'),
             ),
+
             ListTile(
-              leading: Icon(Icons.photo),
-              title: Text('Sample Photo 2'),
-              subtitle: Text('Subtitle 2'),
+              leading: ClipOval(
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1601034913836-a1f43e143611?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+                  width: 40, // Adjust the width to your preference
+                  height: 40, // Adjust the height to your preference
+                  fit: BoxFit.cover, // You can choose the BoxFit that fits your needs
+                ),
+              ),
+              title: const Text('Photo 2'),
+              subtitle: const Text('Description for photo 2'),
             ),
+
+
             ListTile(
-              leading: Icon(Icons.photo),
-              title: Text('Sample Photo 3'),
-              subtitle: Text('Subtitle 3'),
+              leading: ClipOval(
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+                  width: 40, // Adjust the width to your preference
+                  height: 40, // Adjust the height to your preference
+                  fit: BoxFit.cover, // You can choose the BoxFit that fits your needs
+                ),
+              ),
+              title: const Text('Photo 3'),
+              subtitle: const Text('Description for photo 3'),
             ),
-            SizedBox(height: 16.0),
+
+
+
+
+
+            const SizedBox(height: 16.0),
           ],
         ),
       ),
